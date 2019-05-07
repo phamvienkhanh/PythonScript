b2d7f3388386cd2ed6b2d1c05d2fedc4c733a5b4
phamvienkhanh <phamvienkhanh@gmail.ccom>
2019-05-08 00:08:54 +0700

    convert excel into sqldata use sqlalchemy

--- ExcelToSql/scriptExcelToSpl.py
+++ ExcelToSql/scriptExcelToSpl.py
@@ -1,38 +1,33 @@
 import pandas as pd
-import sqlite3 as sql
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
 
-
+#=== Create sql engine ==================================================
+engine = create_engine('sqlite:///data.db', echo=False)
+#========================================================================
 
 #=========================================================================
 xl = pd.ExcelFile("object_prop_matrix.xlsx")
 
-categories = xl.parse(xl.sheet_names[1])
-for row in categories.index:
-    for col in categories.keys():
-        print(categories[col][row])
+df_categories = xl.parse(xl.sheet_names[1])
+df_categories.to_sql('Categories', con=engine, if_exists='replace', index_label='id')
 
+df_ObjectProperties = xl.parse(xl.sheet_names[0])
+df_ObjectProperties.to_sql('ObjectProperties', con=engine, if_exists='replace', index_label='id')
+# for row in categories.index:
+#     line = u''
+#     for col in categories.keys():
+#         line = line + categories[col][row].encode('utf-8') + u' '
+#     print(line)
+
+#print(u'dasd' + u'dasda')
+#print(categories[u'Description '][16].encode('utf-8'))
 #=========================================================================
 
 
 
 #========================================================================
-sqlConnect = sql.connect("data.db")
 
-sqlCur = sqlConnect.cursor()
-
-sqlCur.execute('drop table if exists Categories')
-
-sqlCur.execute('''create table Categories (
-                            ID int primary key not null, 
-                            Name char(50) not null,
-                            Description text,
-                            AnnotationType char(50)
-                        )''')
-
-sqlConnect.commit()
-
-sqlCur.execute('select * from Categories')
-sqlConnect.commit()
-#print(sqlCur.fetchall())
-sqlConnect.close()
 #================================================================
